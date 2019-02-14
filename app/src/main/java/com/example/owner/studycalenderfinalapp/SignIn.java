f0e3485460727e461708bf0c1be84c2d0443f81b
deema aslan <deema@danon.tzafonet.org.il>
2019-02-14 13:53:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/owner/studycalenderfinalapp/SignIn.java
@@ -0,0 +1,100 @@
+package com.example.owner.studycalenderfinalapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    public class loginActivity extends AppCompatActivity {
+        private EditText edEmail, edPassWord;
+        private Button btnLogIN, btnSignUp;
+//        FirebaseAuth auth;//to establish sign in sign up
+//        FirebaseUser user;//user
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_sign_in);
+
+            edEmail = (EditText) findViewById(R.id.edEmail);
+            edPassWord = (EditText) findViewById(R.id.edPass);
+            btnLogIN = (Button) findViewById(R.id.btnLogIn);
+            btnSignUp = (Button) findViewById(R.id.btnSignUp);
+//            auth = FirebaseA.getInstance();
+//            user = auth.getCurrentUser();//
+
+
+//            btnLogIN.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    dataHandler();
+//                }
+//            });
+//            btnSignUp.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    // كود الانتقال إلى الشاشة الأخرى
+//                    Intent i = new Intent(loginActivity.this, SingUpActivity.class);
+//                    startActivity(i);
+//                }
+//            });
+
+        }
+
+
+        private void dataHandler() {
+            boolean isok = true;// if all the fields filled well
+            String email = edEmail.getText().toString();
+            String passWord = edPassWord.getText().toString();
+
+            if (email.length() < 4 ||
+                    email.indexOf('@') < 0 || email.indexOf('.') < 0) {
+                edEmail.setError("Wrong Email");
+                isok = false;
+
+            }
+            if (passWord.length() < 8) {
+                edPassWord.setError("Have to be at least 8 char ");
+                isok = false;
+
+            }
+            if (isok) {
+                SignIn(email, passWord);
+
+            }
+
+        }
+
+        private void SignIn(String email, String PassWord) {
+//            auth.signInWithEmailAndPassword(email, PassWord).addOnCompleteListener(loginActivity.this, new OnCompleteListener<AuthResult>() {
+//                @Override
+//                public void onComplete(@NonNull Task<AuthResult> task) {
+//                    if (task.isSuccessful()) {
+//                        Toast.makeText(loginActivity.this, "signIn successful", Toast.LENGTH_SHORT).show();
+//                        Intent intent = new Intent(loginActivity.this, MainTabsActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    } else {
+//                        Toast.makeText(loginActivity.this, "signIn failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                        task.getException().printStackTrace();
+//                    }
+//                }
+//
+//            });
+        }
+
+
+    }
+}
